595136e8155fd538392da1b45c4870d2ecb03d31
Ratul <ratul38@gmail.com>
2017-04-17 20:35:11 +0600

    Added MainScreen Activity

--- app/src/main/java/com/drbt/energyhackathon/MainActivity.java
+++ app/src/main/java/com/drbt/energyhackathon/MainActivity.java
@@ -1,13 +1,31 @@
 package com.drbt.energyhackathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginBtn,regBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginBtn = (Button)findViewById(R.id.login_btn);
+
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,MainScreenActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
     }
 }
